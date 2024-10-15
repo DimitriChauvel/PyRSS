ff1850beebae767e302d82080ef91c2cabce6f22
DimitriChauvel <chauvel.dimitri@posteo.net>
2024-10-15 21:27:39 +0200

    Manage exception in load_env function

--- src/utils/env.py
+++ src/utils/env.py
@@ -5,8 +5,13 @@ from os import environ
 
 def load_env(file_path: str) -> None:
     """Load environment variables from .env file"""
-    with open(file_path, "r", encoding="utf-8") as file:
-        for line in file:
-            if line.strip() and not line.startswith("#"):
-                key, value = line.strip().split("=", 1)
-                environ[key] = value.replace('"', "")
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                if line.strip() and not line.startswith("#"):
+                    key, value = line.strip().split("=", 1)
+                    environ[key] = value.replace('"', "")
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except PermissionError:
+        print(f"Error: Permission denied when trying to read '{file_path}'.")
